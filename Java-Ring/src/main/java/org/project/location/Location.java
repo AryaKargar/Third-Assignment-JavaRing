78cd0c994731108b0379fde43895f0e404157643
arya <aryakargar85@gmail.com>
2025-03-26 20:00:46 +0330

    Updated Location class

--- Java-Ring/src/main/java/org/project/location/Location.java
+++ Java-Ring/src/main/java/org/project/location/Location.java
@@ -1,32 +1,47 @@
 package org.project.location;
 
 import org.project.entity.enemies.Enemy;
-
 import java.util.ArrayList;
 
 public class Location {
     private String name;
-
     private ArrayList<Enemy> enemies;
 
-    public Location(ArrayList<Location> locations, ArrayList<Enemy> enemies) {
-        this.locations = locations;
+    public Location(String name, ArrayList<Enemy> enemies) {
+        this.name = name;
         this.enemies = enemies;
     }
-
-    /*
-    TODO: (BONUS) RESET EACH LOCATION AFTER PLAYER LEAVES
-    */
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Location> getLocations() {
-        return locations;
-    }
-
     public ArrayList<Enemy> getEnemies() {
         return enemies;
+    }
+
+    public void addEnemy(Enemy enemy) {
+        enemies.add(enemy);
+    }
+
+    public void removeEnemy(Enemy enemy) {
+        enemies.remove(enemy);
+    }
+
+    public void resetLocation(ArrayList<Enemy> newEnemies) {
+        this.enemies = (newEnemies != null) ? newEnemies : new ArrayList<>();
+        System.out.println(name + " has been reset with new enemies!");
+    }
+
+    public void showLocationInfo() {
+        System.out.println("You are at: " + name);
+        if (enemies.isEmpty()) {
+            System.out.println("No enemies here.");
+        } else {
+            System.out.println("Enemies in " + name + ":");
+            for (Enemy enemy : enemies) {
+                System.out.println("- " + enemy.getClass().getSimpleName());
+            }
+        }
     }
 }
